894d898c0f8002a8ed7b91e00766e4329514fa6f
é. urcades <edouard@urcad.es>
2023-08-25 02:34:01 -0400

    Initial Commit

---
+++ FourChannel/FourChannelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourChannelApp.swift
+//  FourChannel
+//
+//  Created by É. Urcades on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FourChannelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
